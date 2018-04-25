e654b93d0bdb958c72b115e5858b230fff902546
Alen Duda <alenduda@gmail.com>
2018-04-25 20:37:20 +0200

    Started frontend development with Vue.js

---
+++ resources/assets/js/components/PropertyList.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="">
+        <h2>Odaberite objekt:</h2>
+        <ul class="list-group">
+            <li v-for="property in properties" class="list-group-item">{{property.name}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function() {
+            return {
+                properties: []
+            }
+        },
+        mounted() {
+            axios.get("properties").then(response =>   {
+                console.log(response.data);
+                this.properties = response.data;
+            });
+        }
+    }
+</script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,71 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="hr">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel</title>
+    <title>Uniline kalkulacija cijene smještaja</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <link rel="stylesheet" href="../public/css/app.css">
+</head>
+<body>
+<div class="container" id="app">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+        <a class="navbar-brand" href="#">
+            <img src="http://static.uniline-cdn.eu/images/2016/logo.png" alt="Uniline logo" height="40px">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDefault"
+                aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            .full-height {
-                height: 100vh;
-            }
+        <div class="collapse navbar-collapse" id="navbarDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Početna <span class="sr-only">(current)</span></a>
+                </li>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Pretraga" aria-label="Pretraga">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Traži</button>
+            </form>
+        </div>
+    </nav>
 
-            .position-ref {
-                position: relative;
-            }
+    <main role="main">
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+        <!-- Main jumbotron for a primary marketing message or call to action -->
+        <div class="jumbotron">
+            <div class="container">
+                <h1 class="display-4">Kalkulacija cijene smještaja</h1>
+                <p class="lead">Turistička agencija nudi gostima smještaj u velikom broju smještajnih objekata, koji
+                    mogu imati više smještajnih jedinica, kao što su apartmani, sobe ili kompletne kuće. Objekti i njihove jedinice
+                    imaju različite osobine, mogu primiti različit broj osoba, pripadaju različitim vlasnicima, te se
+                    naravno smještaj u njima plaća po različitim cijenama. Osim osnovne cijene, cjenici turističkog
+                    smještaja obično sadrže popuste i doplate, a u izračunu konačnog iznosa potrebno je obratiti
+                    pozornost i na druge stavke, kao što je npr. boravišna taksa.</p>
             </div>
         </div>
-    </body>
+
+
+        <property-list></property-list>
+
+
+    </main>
+
+</div>
+
+<script type="text/javascript" src="../public/js/app.js"></script>
+<style>
+    body {
+        padding-top: 5rem;
+    }
+</style>
+</body>
 </html>
