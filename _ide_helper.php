d806c1320588994b7bd73c6ebeb2b626c304e748
Alen Duda <alenduda@gmail.com>
2018-05-21 20:20:55 +0200

    Added CORS support

--- _ide_helper.php
+++ _ide_helper.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 5.6.17 on 2018-04-25 11:55:32.
+ * Generated for Laravel 5.6.17 on 2018-05-21 17:32:39.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
